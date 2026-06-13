62e17742b410266e1e4e5be657c2e36cab8e9341
saparaliyevshaxzodbek-dot <saparaliyevshaxzodbek@gmail.com>
2026-06-13 16:23:37 +0500

    add

--- Exam/src/Exam6_Modul.Api/Entitiys/Category.cs
+++ Exam/src/Exam6_Modul.Api/Entitiys/Category.cs
@@ -1,6 +1,12 @@
-﻿namespace Exam6_Modul.Api.Entitiys
+﻿namespace Exam6_Modul.Api.Entitiys;
+
+public class Category
 {
-    public class Category
-    {
-    }
+    public int CategoryId { get; set; }
+
+  
+    public string Name { get; set; } = string.Empty;
+
+   
+    public ICollection<Food> Foods { get; set; } = new List<Food>();
 }
---
+++ Exam/src/Exam6_Modul.Api/Entitiys/Food.cs
@@ -0,0 +1,24 @@
+﻿namespace Exam6_Modul.Api.Entitiys;
+
+public class Food
+{
+    public int FoodId { get; set; }
+
+    
+    public string Name { get; set; } = string.Empty;
+
+  
+    public string? Description { get; set; }
+
+    
+    public decimal Price { get; set; }
+
+    
+    public bool IsAvailable { get; set; }
+
+  
+    public int CategoryId { get; set; }
+
+    
+    public Category Category { get; set; } = null!;
+}
