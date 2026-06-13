fca7adeee3dde45c2083e344ecfdbd52d989e0ec
rasullbe <rasulbek112208@gmail.com>
2026-06-13 16:53:26 +0500

    zor

--- Exam/src/Exam6_Modul.Api/Program.cs
+++ Exam/src/Exam6_Modul.Api/Program.cs
@@ -1,3 +1,6 @@
+using Exam6_Modul.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Exam6_Modul.Api;
 
 public class Program
@@ -6,14 +9,26 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        // Configure DbContext with SQL Server
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+            ?? "Server=(localdb)\\mssqllocaldb;Database=Exam6ModulDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connectionString));
 
+        // Add services to the container.
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
+
+        // Apply migrations on startup
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.Migrate();
+        }
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
